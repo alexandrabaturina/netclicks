85410bed38e2de3fc9408dd0edd7b1bfab1a9ca9
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-05-26 16:22:14 -0700

    feat: render card

--- script/main.js
+++ script/main.js
@@ -105,4 +105,42 @@ const DBService = class {
     }
 }
 
+// Render card
+const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+
+const renderCard = response => {
+    console.log(response);
+    // Clear list
+    tvShowsList.textContent = '';
+    response.results.forEach(item => {
+
+        const {
+            backdrop_path: backdrop,
+            name: title,
+            poster_path: poster,
+            vote_average: vote
+        } = item;
+
+        // If no poster, render img/no-poster.jpg
+        const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
+        const backdropIMG = '';
+        const voteElem = '';
+
+
+        const card = document.createElement('li');
+        card.classList.add('tv-shows__item');
+        card.innerHTML = `
+            <a href="#" class="tv-card">
+                <span class="tv-card__vote">${vote}</span>
+                <img class="tv-card__img"
+                    src="${posterIMG}"
+                    data-backdrop="${IMG_URL + backdrop}"
+                    alt="${title}">
+                <h4 class="tv-card__head">${title}</h4>
+            </a>
+        `;
+        tvShowsList.append(card);
+    });
+
+};
 new DBService().getTestData().then(renderCard);
